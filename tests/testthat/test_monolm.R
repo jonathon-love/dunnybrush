37f93114b9a555e845f227df7b0c03333acac52d
Jonathon Love <jon@thon.cc>
2023-02-20 11:51:48 +1100

    Added more detailed unit tests

--- tests/testthat/test_monolm.R
+++ tests/testthat/test_monolm.R
@@ -17,6 +17,14 @@ testthat::test_that('it works!', {
 
   # results <- monolm(testdata, Y ~ A * B)
 
+  expect_equal(results[1, 'Mean.Sq'], 3.458)
+  expect_equal(results[2, 'Mean.Sq'], 1.135)
+  expect_equal(results[3, 'Mean.Sq'], 6.012)
+  expect_equal(results[4, 'Mean.Sq'], 2.062)
+  expect_equal(results[5, 'Mean.Sq'], 0.865)
+
+  expect_equal(results[3, 'p.value'], 0.01, tolerance=.03)
+  expect_equal(results[4, 'p.value'], 0.07, tolerance=.05)
   data <- ToothGrowth
   # data <- subset(data, data$dose != 0.5)
   data$dose <- as.factor(data$dose)
